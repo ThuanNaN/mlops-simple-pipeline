e48c1bede920236e0fac537ab90dcac16ca88d3d
ThuanNaN <duongthuan1445@gmail.com>
2024-03-05 07:38:45 +0700

    fix prefix model name

--- backend/src/model_training.py
+++ backend/src/model_training.py
@@ -1,6 +1,6 @@
 import argparse
 import torchvision
-from modeling import create_resnet, Trainer
+from modeling import Trainer, create_mobilenet, create_resnet
 from utils import DataPath, CatDog_Data, Log, seed_everything
 
 logger = Log(__file__).get_logger()
@@ -10,7 +10,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_version", type=str, required=True)
-    parser.add_argument("--model_name", type=str, default="resnet18")
+    parser.add_argument("--model_name", type=str, default="resnet_18",
+                        choices=["resnet_18", "resnet_34", "mobilenet_v2", "mobilenet_v3_small"])
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--weight_decay", type=float, default=1e-5)
@@ -18,8 +19,14 @@ if __name__ == "__main__":
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--seed", type=int, default=43)
     args = parser.parse_args()
-
     seed_everything(args.seed)
+
+    try:
+        data_path = DataPath.TRAIN_DATA_DIR/args.data_version
+        assert data_path.exists()
+    except AssertionError:
+        logger.error(f"Data version: {args.data_version} not found.")
+        raise FileNotFoundError(f"Data version: {args.data_version} not found.")
 
     train_data = torchvision.datasets.ImageFolder(
         root=DataPath.TRAIN_DATA_DIR/args.data_version/"train",
@@ -35,8 +42,12 @@ if __name__ == "__main__":
         transform=CatDog_Data.test_transform
     )
 
-    model = create_resnet(n_classes=CatDog_Data.n_classes, model_name=args.model_name, load_pretrained=False)
-
+    model_prefix = args.model_name.split("_")[0]
+    if model_prefix == "resnet":
+        model = create_resnet(n_classes=CatDog_Data.n_classes, model_name=args.model_name)
+    elif model_prefix == "mobilenet":
+        model = create_mobilenet(n_classes=CatDog_Data.n_classes, model_name=args.model_name)
+    
     mlflow_log_pamrams = {
         "data_version": args.data_version,
         "model": model.__class__.__name__,
@@ -62,4 +73,7 @@ if __name__ == "__main__":
                       verbose=True)
 
     trainer.train()
+
+    logger.info(f"Model Training Completed. Model: {args.model_name}, Data: {args.data_version}")
+
 
